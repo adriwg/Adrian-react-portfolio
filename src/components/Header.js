8a996bf33d8fef3e563d16cb41529843f4d31a54
AdrianWong <adrianwg04@gmail.com>
2023-05-19 19:19:55 +0100

    Added components start up files, added routing in App.js, added Header.css

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../css/Header.css';
+
+const Header = () => {
+  return (
+    <div className="nav_bar">
+    <ul className="nav justify-content-end">
+        <li className="nav-item">
+            <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Home</NavLink>
+        </li>
+        <li className="nav-item">
+            <NavLink to="projects" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Projects</NavLink>
+        </li>
+        <li className="nav-item">
+            <NavLink to="contact" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Contact</NavLink>
+        </li>
+    </ul>
+    </div>
+  )
+}
+
+export default Header
