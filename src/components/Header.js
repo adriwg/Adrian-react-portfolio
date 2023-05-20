131a3a760bcdfebc68987df2c37c89f018f75d1c
AdrianWong <adrianwg04@gmail.com>
2023-05-20 15:15:06 +0100

    Styling the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-//import '../css/Header.css';
+import '../css/Header.css';
 
 const Header = () => {
   return (
     <div className="nav_bar">
-    <ul className="nav justify-content-end">
-        <li className="nav-item">
-            <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Home</NavLink>
-        </li>
-        <li className="nav-item">
-            <NavLink to="projects" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Projects</NavLink>
-        </li>
-        <li className="nav-item">
-            <NavLink to="contact" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Contact</NavLink>
-        </li>
-    </ul>
+      <div className="container d-flex justify-content-between">
+        <div className="logoType">Adrian's <span>Portfolio</span></div>
+        <ul className="nav">
+          <li className="nav-item">
+              <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Home</NavLink>
+          </li>
+          <li className="nav-item">
+              <NavLink to="projects" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Projects</NavLink>
+          </li>
+          <li className="nav-item">
+              <NavLink to="contact" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    
     </div>
   )
 }
