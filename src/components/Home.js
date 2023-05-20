610b87d027e13502bdf91ae818dd8a919684b9dc
AdrianWong <adrianwg04@gmail.com>
2023-05-20 17:49:09 +0100

    Finished component Home.js, added Home.css and also added background image for the hero banner of the home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import '../css/Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <section className="jumbotron">
+        <div className="container hero-banner">
+          <div className="nameWrapper">
+              <div className="hi">Hi, I'm</div>
+              <div className="name">Adrian Wong</div>
+              <div className="title">UI Designer and Front-end Developer</div>
+          </div>
+        <div className="profilePic_holder"></div>
+        </div>
+      </section>
+      <section className="section_content_home">
+        <div className="container">
+          <p>
+          I possess strong skills in transforming ideas into visually stunning and intuitive design solutions. Leveraging my expertise in modern and responsive front-end development, I specialize in elevating user interfaces to deliver seamless and engaging experiences.
+          </p>
+      <button type="button" className="btn-default" onclick="">Get in Touch</button>
+        </div>
+      </section>
+    </div>
   )
 }
 
