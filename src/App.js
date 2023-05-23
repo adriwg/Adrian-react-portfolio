87df47257fef99fa2154ef5af9de32209fae5514
AdrianWong <adrianwg04@gmail.com>
2023-05-23 13:22:55 +0100

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="projects/*" element={<Projects />} />
+        <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
     </div>
